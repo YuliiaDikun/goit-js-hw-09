c1bf597cf3615a8508f0a38d36dd3e2ed8179571
Yuliia Dikun <dikun.yulia@gmail.com>
2022-11-08 20:19:45 +0200

    prettier formatting

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,7 +2,6 @@ const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 const body = document.querySelector('body');
 
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
@@ -13,17 +12,17 @@ stopBtn.addEventListener('click', stopBtnEvent);
 let interval;
 stopBtn.disabled = true;
 
-function startBtnEvent() { 
+function startBtnEvent() {
   console.log('this is startBtnEvent');
   startBtn.disabled = true;
   stopBtn.disabled = false;
-  interval = setInterval(() => {     
+  interval = setInterval(() => {
     let color = getRandomHexColor();
     body.style.backgroundColor = color;
-  }, 1000)
+  }, 1000);
 }
 
-function stopBtnEvent() { 
+function stopBtnEvent() {
   console.log('this is stopBtn event');
   startBtn.disabled = false;
   stopBtn.disabled = true;
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,5 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 
 const inputEl = document.querySelector('input#datetime-picker');
 const startBtn = document.querySelector('[data-start]');
@@ -19,41 +19,40 @@ flatpickr(inputEl, {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-      console.log(selectedDates[0]);
-      const today = new Date();
-      choosenDate = selectedDates[0];
-      if (selectedDates[0].getTime() < today.getTime()) {
-          alert('Please choose a date in the future');
-      } else { 
-          startBtn.disabled = false;          
-      }
-    },
+    console.log(selectedDates[0]);
+    const today = new Date();
+    choosenDate = selectedDates[0];
+    if (selectedDates[0].getTime() < today.getTime()) {
+      alert('Please choose a date in the future');
+    } else {
+      startBtn.disabled = false;
+    }
+  },
 });
-  
+
 startBtn.addEventListener('click', startBtnEvent);
 
-function startBtnEvent() { 
-    
-    startBtn.disabled = true;
-    inputEl.disabled = true;
+function startBtnEvent() {
+  startBtn.disabled = true;
+  inputEl.disabled = true;
 
-    const timeInterval = setInterval(updateTimer, 1000);    
+  const timeInterval = setInterval(updateTimer, 1000);
 
-    function updateTimer() {
-        const today = new Date();
-        const time = choosenDate - today;
-        
-        const dateObj = convertMs(time);       
-        
-        dateEl.innerHTML = addLeadingZero(dateObj.days);
-        hoursEl.innerHTML = addLeadingZero(dateObj.hours);
-        minutesEl.innerHTML = addLeadingZero(dateObj.minutes);
-        secondsEl.innerHTML = addLeadingZero(dateObj.seconds);
-        
-        if (dateObj <= 0) { 
-            clearInterval(timeInterval);
-        }
+  function updateTimer() {
+    const today = new Date();
+    const time = choosenDate - today;
+
+    const dateObj = convertMs(time);
+
+    dateEl.innerHTML = addLeadingZero(dateObj.days);
+    hoursEl.innerHTML = addLeadingZero(dateObj.hours);
+    minutesEl.innerHTML = addLeadingZero(dateObj.minutes);
+    secondsEl.innerHTML = addLeadingZero(dateObj.seconds);
+
+    if (dateObj <= 0) {
+      clearInterval(timeInterval);
     }
+  }
 }
 
 function convertMs(ms) {
@@ -76,10 +75,9 @@ function convertMs(ms) {
 }
 
 function addLeadingZero(n) {
-    if(n >= 0 && n < 10) {
-        return `0${n}`;
-    } else {
-        return n;
-    }
+  if (n >= 0 && n < 10) {
+    return `0${n}`;
+  } else {
+    return n;
+  }
 }
-
