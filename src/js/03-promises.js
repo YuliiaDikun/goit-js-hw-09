4c084579c0913b25128a53f0ef836e7e1b9b360f
Yuliia Dikun <dikun.yulia@gmail.com>
2022-11-09 20:20:43 +0200

    third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,31 @@
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const delay = Number(formEl.elements.delay.value);
+  const step = Number(formEl.elements.step.value);
+  const amount = Number(formEl.elements.amount.value);
+  for (let i = 0; i < amount; i++) {
+    setTimeout(() => {
+      const position = i + 1;
+      const delayProm = delay + step * i;
+      createPromise(position, delayProm)
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
+    }, delay + step * i);
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      return resolve({ position, delay });
+    } else {
+      reject('Error!');
+    }
+  });
+  return promise;
 }
